c31d884e2c5bd1ad3b17fcba1c0cf6e9c516f80b
ujjawalkashyap <ujjawalkashyap1020@gmail.com>
2025-10-04 09:56:19 +0530

    main file

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 package src;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println("test");
         System.out.println("test2");
 
