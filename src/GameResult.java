d440fc8020272d872dfa3737310d8aac46ba7a3a
ujjawalkashyap <ujjawalkashyap1020@gmail.com>
2025-10-04 18:58:51 +0530

    moved the code to separate classes

---
+++ src/GameResult.java
@@ -0,0 +1,11 @@
+package src;
+
+class GameResult {
+    boolean isComplete;
+    String winner;
+
+    GameResult(boolean isComplete, String winner) {
+        this.isComplete = isComplete;
+        this.winner = winner;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,96 @@
 package src;
+import src.Board;
+import src.GameResult;
+import src.Move;
+import src.Player;
+import src.TicTacToeBoard;
 
 public class Main {
     public static void main(String[] args){
         System.out.println("test");
         System.out.println("test2");
+    }
+
+    public Board start(){
+        return new Board();
+    }
+
+    public void move(Board board, Move move, Player player){
 
     }
+
+    public GameResult isCompleted(Board board){
+        if(board instanceof TicTacToeBoard){
+            TicTacToeBoard board1 = (TicTacToeBoard) board;
+            boolean rowComplete = true;
+            String firstCharacter = "-";
+            for(int i=0;i<3;i++){
+                rowComplete = true;
+                firstCharacter = board1.cells[i][0];
+                for(int j=0;j<3;j++){
+                    if(!board1.cells[i][j].equals(firstCharacter)){
+                        rowComplete = false;
+                        break;
+                    }
+                }
+            }
+
+            if(rowComplete){
+                return new GameResult(true, firstCharacter);
+            }
+            boolean colComplete =true;
+            for(int i=0;i<3;i++){
+                colComplete = true;
+                firstCharacter = board1.cells[0][i];
+                for(int j=1;j<3;j++){
+                    if(!board1.cells[j][i].equals(firstCharacter)){
+                        colComplete = false;
+                        break;
+                    }
+                }
+            }
+            if(colComplete){
+                return new GameResult(true, firstCharacter);
+            }
+            boolean DiagonalComplete = true;
+            firstCharacter = board1.cells[0][0];
+            for(int j=1;j<3;j++){
+                if(!board1.cells[j][j].equals(firstCharacter)){
+                    DiagonalComplete = false;
+                    break;
+                }
+            }
+            if(DiagonalComplete){
+                return new GameResult(true, firstCharacter);
+            }
+            boolean revDiagonalComplete = true;
+            firstCharacter = board1.cells[0][2];
+            for(int j=1;j<3;j++){
+                if(!board1.cells[j][2-j].equals(firstCharacter)){
+                    revDiagonalComplete = false;
+                    break;
+                }
+            }
+            if(revDiagonalComplete){
+                return new GameResult(true, firstCharacter);
+            }
+
+            int numberOfFilledCells = 0;
+            for(int i=0;i<3;i++){
+                for(int j=0;j<3;j++){
+                    if(board1.cells[i][j].equals("-")){
+                        numberOfFilledCells++;
+                    }
+                }
+            }
+            if(numberOfFilledCells==9){
+                return new GameResult(true, "-");
+            }
+            else{
+                return new GameResult(false, "-");
+            }
+        }
+        else return new GameResult(false, "-");
+    }
 }
+
